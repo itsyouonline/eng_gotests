8dc3b15230daaed654de212d8380ae1ce133ede9
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-04-11 08:41:17 +0000

    Add command line option

--- mmap_capnp/main.go
+++ mmap_capnp/main.go
@@ -1,175 +1,43 @@
 package main
 
 import (
-	"bytes"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"syscall"
+)
 
-	"zombiezen.com/go/capnproto2"
+var (
+	mmapBulk bool
+	mmapOne  bool
 )
 
 func main() {
+	flag.BoolVar(&mmapBulk, "mmap-bulk", false, "write bulk and read bulk")
+	flag.BoolVar(&mmapOne, "mmap-one", false, "write one and read one")
+
+	flag.Parse()
+
+	if !mmapBulk && !mmapOne {
+		fmt.Println("please specify test to perform")
+		fmt.Println("run with '-h' option to see all available tests")
+		return
+	}
+
 	num := 1000 * 1000
-	if err := writeBulkRead(num); err != nil {
-		log.Printf("err = %v\n", err)
-	}
 
-	if err := writeOneReadOne(num, 70); err != nil {
-		log.Printf("writeOneReadOn err = %v\n", err)
-	}
-}
-
-// for 0..num
-// 	- encode canpnp message
-//  - add it to mmap'ed file
-//  - read some written capnp message
-func writeOneReadOne(num, blockSize int) error {
-	log.Printf("====== for i...%v {write one capnp doc, read one capnp doc} ========= \n", num)
-	// create mem mapped file
-	f, data, err := createMemMap(num * blockSize)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	defer syscall.Munmap(data)
-
-	for i := 0; i < num; i++ {
-		// write block
-		if err := writeBlock(data, i, blockSize); err != nil {
-			return err
+	if mmapBulk {
+		if err := writeBulkRead(num); err != nil {
+			log.Printf("err = %v\n", err)
 		}
+	}
 
-		if i < 5 { // too early to read
-			continue
+	if mmapOne {
+		if err := writeOneReadOne(num, 70); err != nil {
+			log.Printf("writeOneReadOn err = %v\n", err)
 		}
-
-		// decode some block
-		readIdx := i / 2
-
-		decodedBlock, err := decodeBlock(data, readIdx, blockSize)
-		if err != nil {
-			log.Printf("failed to decode block:%v\n", err)
-			return err
-		}
-
-		// check it's values
-		checkBlockVal(decodedBlock, readIdx)
 	}
-	log.Println("all  good")
-	return nil
-}
-
-func decodeBlock(data []byte, i, blockSize int) (*TlogBlock, error) {
-	start := i * blockSize
-	end := start + blockSize
-	buf := bytes.NewBuffer(data[start:end])
-
-	msg, err := capnp.NewDecoder(buf).Decode()
-	if err != nil {
-		return nil, err
-	}
-
-	block, err := ReadRootTlogBlock(msg)
-	return &block, err
-}
-func writeBlock(data []byte, i, blockSize int) error {
-	// create block
-	msg, seg, err := capnp.NewMessage(capnp.SingleSegment(nil))
-	if err != nil {
-		return err
-	}
-	block, err := NewRootTlogBlock(seg)
-	if err != nil {
-		return err
-	}
-	setBlockVal(&block, i)
-
-	// add it to mmap'ed file
-	start := i * blockSize
-	end := start + blockSize
-	buf := bytes.NewBuffer(data[start:end])
-	buf.Truncate(0)
-
-	return capnp.NewEncoder(buf).Encode(msg)
-}
-
-// - encode 1M capnp message & write it to mem mapped file
-// - read some data
-func writeBulkRead(num int) error {
-	log.Printf("======== create %v capnp messages and write it to mem mapped file ==========\n", num)
-
-	// create the capnp aggregation object
-	aggMsg, aggSeg, err := capnp.NewMessage(capnp.SingleSegment(nil))
-	if err != nil {
-		return err
-	}
-	agg, err := NewRootTlogAggregation(aggSeg)
-	if err != nil {
-		return err
-	}
-	agg.SetName("the 1 M message")
-	agg.SetSize(0)
-	blockList, err := agg.NewBlocks(int32(num))
-	if err != nil {
-		return err
-	}
-
-	// add blocks
-	for i := 0; i < blockList.Len(); i++ {
-		block := blockList.At(i)
-		setBlockVal(&block, i)
-	}
-
-	agg.SetSize(uint64(num))
-
-	size := countMemSize(num)
-
-	// create mem mapped file
-	f, data, err := createMemMap(size)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	defer syscall.Munmap(data)
-	buf := bytes.NewBuffer(data)
-	buf.Truncate(0)
-
-	// write capnp msg to mem mapped file
-	if err := capnp.NewEncoder(buf).Encode(aggMsg); err != nil {
-		return err
-	}
-
-	// read it again
-	_, decodedBlockList, err := decodeAggBlocks(buf)
-	if err != nil {
-		log.Printf("failed to decode block list:%v", err)
-		return err
-	}
-
-	for i := 0; i < num; i++ {
-		block := decodedBlockList.At(i)
-		// do some checking
-		checkBlockVal(&block, i)
-	}
-
-	log.Println("all  good")
-
-	return nil
-}
-
-func decodeAggBlocks(buf *bytes.Buffer) (*TlogAggregation, *TlogBlock_List, error) {
-	msg, err := capnp.NewDecoder(buf).Decode()
-	if err != nil {
-		return nil, nil, err
-	}
-	agg, err := ReadRootTlogAggregation(msg)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	blocks, err := agg.Blocks()
-	return &agg, &blocks, err
 }
 
 func setBlockVal(block *TlogBlock, val int) {
