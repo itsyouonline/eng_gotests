501a5832c6273292a3160637970d946a2733a982
Iwan Budi Kusnanto <iwanbk@gmail.com>
2017-04-12 14:13:22 +0000

    multiple clients

--- tarantool_perf/main.go
+++ tarantool_perf/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"runtime"
+	"sync"
 	"time"
 
 	"github.com/tarantool/go-tarantool"
@@ -18,26 +20,24 @@ func main() {
 	var passwd string
 	var addr string
 	var num int
+	var numClient int
 
 	flag.StringVar(&user, "user", "", "tarantool username")
 	flag.StringVar(&passwd, "passwd", "", "tarantool password")
 	flag.StringVar(&addr, "addr", "127.0.0.1:3301", "tarantool server address (default: 127.0.0.1:3301)")
 	flag.IntVar(&num, "num", 1000*1000, "number of messages to store")
+	flag.IntVar(&numClient, "num-client", runtime.NumCPU(), "number of clients (default:number of cpu cores)")
 
 	flag.Parse()
 
 	fmt.Println("test data:")
 	fmt.Printf("\t number of messages:%v\n", num)
 	fmt.Printf("\t data len = %v bytes\n", dataLen)
+	fmt.Printf("\t number of clients = %v\n", numClient)
 
 	opts := tarantool.Opts{
 		User: user,
 		Pass: passwd,
-	}
-
-	conn, err := tarantool.Connect(addr, opts)
-	if err != nil {
-		log.Fatalf("Connection refused: %s", err)
 	}
 
 	space := "xxx"
@@ -50,15 +50,48 @@ func main() {
 
 	start := time.Now()
 
-	for i := 0; i < num; i++ {
-		resp, err := conn.Insert(space, []interface{}{i, data})
-		if err != nil {
-			log.Fatalf("Code:%v, err:%v", resp.Code, err)
+	idChan := make(chan int, numClient)
+	closeChan := make(chan bool)
+	go func() {
+		for i := 0; i < num; i++ {
+			idChan <- i
 		}
+		close(closeChan)
+	}()
+
+	var wg sync.WaitGroup
+	for i := 0; i < numClient; i++ {
+		wg.Add(1)
+		go func() {
+			conn, err := tarantool.Connect(addr, opts)
+			if err != nil {
+				log.Fatalf("Connection refused: %s", err)
+			}
+
+			defer wg.Done()
+			for {
+				select {
+				case idx := <-idChan:
+					resp, err := conn.Insert(space, []interface{}{idx, data})
+					if err != nil {
+						log.Fatalf("Code:%v, err:%v", resp.Code, err)
+					}
+				case <-closeChan:
+					return
+				}
+			}
+		}()
 	}
+	wg.Wait()
+
 	fmt.Printf("\t time needed:%v seconds\n", time.Since(start).Seconds())
 
 	// test getting some data we have stored
+	conn, err := tarantool.Connect(addr, opts)
+	if err != nil {
+		log.Fatalf("Connection refused: %s", err)
+	}
+
 	resp, err := conn.Select(space, index, 0, 1, tarantool.IterEq, []interface{}{0})
 	if err != nil {
 		log.Fatalf("Code:%v, err:%v", resp.Code, err)
